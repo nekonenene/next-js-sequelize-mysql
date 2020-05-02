308be7340436bca1ea772cf0d94cf87040ef140d
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-05-02 09:22:22 +0900

    Add layout.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <div>Welcome to Next.js!</div>
+    </Layout>
+  )
 }
 
 export default HomePage
