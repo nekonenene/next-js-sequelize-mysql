5aaf2aface95bdaef305c9e253a3ddcdf88cc54e
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-05-02 09:58:00 +0900

    Set AppBar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,29 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
 
 export const siteTitle = 'Sample Website'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    content: {
+      flexGrow: 1,
+      height: '100vh',
+      overflow: 'auto',
+    },
+  }),
+);
 
 export default function Layout({
   children,
@@ -10,11 +32,13 @@ export default function Layout({
   children: React.ReactNode
   home?: boolean
 }) {
+  const classes = useStyles();
+
   return (
     <div>
       <Head>
         <meta charSet="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="author" content="nekonenene" />
         <link rel="shortcut icon" href="/favicon.ico" />
@@ -31,10 +55,20 @@ export default function Layout({
         <meta name="twitter:card" content="summary_large_image" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
       </Head>
-      <header>
-        Hello
-      </header>
-      <main>{children}</main>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>{siteTitle}</Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <main className={classes.content}>
+        {children}
+      </main>
       {!home && (
         <div>
           <Link href="/">
