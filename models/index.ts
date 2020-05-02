0b88009051a2aeddccb84c417ff790f776836752
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-05-03 08:32:10 +0900

    Use Article in models/index.ts

--- models/index.ts
+++ models/index.ts
@@ -1,11 +1,8 @@
-import fs from 'fs';
-import path from 'path';
 import Sequelize from 'sequelize';
+import Article from './Article';
 
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-let db = {};
 
 let sequelize: Sequelize.Sequelize;
 if (config.use_env_variable) {
@@ -14,27 +11,14 @@ if (config.use_env_variable) {
   sequelize = new Sequelize.Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+const db = {
+  Article: Article.initialize(sequelize),
+};
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-
-const sequelizeHash = {
-  sequelize: sequelize,
-  Sequelize: Sequelize,
-}
-
-db = { ...db, ...sequelizeHash }
 
 module.exports = db;
