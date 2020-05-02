fa4e67ad89306647e7d1f932ffa76bf203a34624
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-05-03 07:21:07 +0900

    index.js -> index.ts

--- models/index.ts
+++ models/index.ts
@@ -1,18 +1,17 @@
-'use strict';
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+let db = {};
 
-let sequelize;
+let sequelize: Sequelize.Sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize.Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize.Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
@@ -31,7 +30,11 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+const sequelizeHash = {
+  sequelize: sequelize,
+  Sequelize: Sequelize,
+}
+
+db = { ...db, ...sequelizeHash }
 
 module.exports = db;
